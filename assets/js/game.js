4956aab78d04fc072aa69c69a2ae964f3c46faa5
Tony Slonaker <tony.slonaker@gmail.com>
2021-08-01 14:01:21 -0600

    update js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("this is an alert! javaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+    
+    var playerName = window.prompt("What is your robot's name?");
+    // Note the lack of quotation marks around playerName
+    // What is this?
+    console.log(playerName);
+   //window.alert(playerName);
+
+    console.log("This logs a string, good for laeaving yourself a message");
+    // this will do math and log 20
+    console.log(10 + 10);
+    //what is this?
+    console.log("Our robot's name is " + playerName);
+}
+
+// fight();
+
