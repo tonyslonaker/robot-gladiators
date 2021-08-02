b3edc0bf29b851e2a6036ee217d2f2024fe61fee
Tony Slonaker <tony.slonaker@gmail.com>
2021-08-02 16:30:59 -0600

    update code per lesson 3.3.3

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,11 +70,14 @@ var fight = function(enemyName) {
 };
 
 // fight each enemy-robot by looping over them and fighting them one at a time
+// function to start a new game
+var startGame = function() {
+
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
   if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
     // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
@@ -92,5 +95,13 @@ for (var i = 0; i < enemyNames.length; i++) {
   else {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
+
+    //play again
+startGame();
+
+    }
   }
-}
+};
+
+// start the game when the page loads
+startGame();
